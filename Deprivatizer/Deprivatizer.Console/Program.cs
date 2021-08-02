998374d9834fd7259484562a2698983fbad3b44b
Mariusz Klimek <klimcio@gmail.com>
2021-08-02 21:46:49 +0200

    Initial commit

---
+++ Deprivatizer/Deprivatizer.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Deprivatizer.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
